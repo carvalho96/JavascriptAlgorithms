10761a3e338a749f5f41adceda00c08c94726e0d
Matheus Carvalho <matheusgmc@hotmail.com>
2019-02-20 18:10:56 -0300

    Initial folder and MergeSort structure

---
+++ src/main/scala/sort/MergeSort.scala
@@ -0,0 +1,27 @@
+package sort
+
+object MergeSort {
+
+  /*
+   *
+   * PARAM: Array with of integers
+   * RETURN: Sorted array
+   *
+   */
+
+  def mergeSort(array: Array[Int]): Array[Int] = {
+
+    def startSorting (array: Array[Int]): Array[Int] = {
+    }
+
+    def divideArray (array: Array[Int], low: Int, high: Int): Array[Int] = {
+
+    }
+
+    def mergeArray (array: Array[Int], low: Int, mid: Int, high: Int): Array[Int] = {
+
+    }
+
+  }
+
+}
