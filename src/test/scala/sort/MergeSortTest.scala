3b078c89e6bca9fcf31b211d2a8e3a801c2994d8
Matheus Carvalho <matheusgmc@hotmail.com>
2019-02-26 17:41:32 -0300

    Update test case to use only two lines of print func

--- src/test/scala/sort/MergeSortTest.scala
+++ src/test/scala/sort/MergeSortTest.scala
@@ -7,13 +7,13 @@ class MergeSortTest extends FlatSpec {
 
   "The Merge Sort" should "return sorted given array" in {
     val arr = Array(13,2,8,3,25,0)
-    print("Input Array:\t[")
-    print(arr.deep.mkString(", "))
-    println("]")
+
+    println("Input Array:\t[" + arr.deep.mkString(", ") +"]")
+
     assert(MergeSort.mergeSort(arr) === Array(0,2,3,8,13,25))
-    print("Output Array:\t[")
-    print(arr.deep.mkString(", "))
-    println("]")
+
+    println("Input Array:\t[" + arr.deep.mkString(", ") +"]")
+    
   }
 
 }
